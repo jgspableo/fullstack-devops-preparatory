eabb98a0d2b863e58342fc3ef09a6a56c4f8a79e
Julius Pableo <jgspableo@outlook.com>
2025-11-28 18:50:49 +0800

    Added packages lab

---
+++ Python/Packages/geometry.py
@@ -0,0 +1,14 @@
+
+def area_of_rectangle(length, width):
+    """
+    This function returns the area of a rectangle given its length and width
+    """
+    return length * width
+
+def area_of_circle(radius):
+    """
+    This function returns the area of a circle given its radius
+    """
+    import math
+    return math.pi * radius ** 2
+
