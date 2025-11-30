07259bab105574716a9e57038bdd97ee2712e124
jgspableo <jgspableo@outlook.com>
2025-11-30 22:09:38 +0800

    Added python final project lab

---
+++ Python/FINAL PROJECT/server.py
@@ -0,0 +1,51 @@
+"""
+Executing this function initiates the application of emotion
+detection to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Initiate the flask app
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    """
+    This code receives the text from the HTML interface and 
+    runs emotion detection over it using emotion_detector()
+    function. The output returned shows the label and its confidence 
+    score for the provided text.
+    """
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract the dominant emotion
+    dominant_emotion = response['dominant_emotion']
+
+    # Check if the dominant emotion is None
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    # Return the formatted string
+    return (
+        f"For the given statement, the system response is 'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+
+@app.route("/")
+def render_index_page():
+    """
+    This function initiates the rendering of the main application
+    page over the Flask channel
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5000)
