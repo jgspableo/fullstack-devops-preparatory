5f9626baa32c5bd771ed0b3da72d8115bcc6f905
Julius Pableo <jgspableo@outlook.com>
2026-01-16 20:51:54 +0800

    Add forms exercise: sprint 2

--- full-stack/Odin Projects/full-stack-js/node_express/formsExample/controllers/userController.js
+++ full-stack/Odin Projects/full-stack-js/node_express/formsExample/controllers/userController.js
@@ -1,4 +1,5 @@
 const usersStorage = require("../storages/userStorage");
+const { body, validationResult, matchedData } = require("express-validator");
 
 exports.usersListGet = (req, res) => {
   res.render("index", {
@@ -13,8 +14,89 @@ exports.usersCreateGet = (req, res) => {
   });
 };
 
-exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+const emailErr = "must be a valid email.";
+const ageErr = "must be a number between 18 and 120";
+const bioErr = "has a maximum of 200 characters";
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First Name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("email").trim().isEmail().withMessage(`Email ${emailErr}`),
+  body("age").trim().isInt({ min: 18, max: 120 }).withMessage(`Age ${ageErr}`),
+  body("bio").trim().isLength({ max: 200 }).withMessage(`Bio ${bioErr}`),
+];
+
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create User",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName, email, age, bio } = matchedData(req);
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
+    res.redirect("/");
+  },
+];
+
+exports.usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
+};
+
+exports.usersUpdatePost = [
+  validateUser,
+  (req, res) => {
+    const user = usersStorage.getUser(req.params.id);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user: user,
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName, email, age, bio } = matchedData(req);
+    usersStorage.updateUser(req.params.id, {
+      firstName,
+      lastName,
+      email,
+      age,
+      bio,
+    });
+    res.redirect("/");
+  },
+];
+
+exports.usersDeletePost = (req, res) => {
+  usersStorage.deleteUser(req.params.id);
   res.redirect("/");
+};
+
+exports.userSearchGet = (req, res) => {
+  const query = req.query.search;
+  const usersMatch = usersStorage.findUsers(query);
+  res.render("searchUser", {
+    title: "Search Results",
+    query: query,
+    users: usersMatch,
+  });
 };
--- full-stack/Odin Projects/full-stack-js/node_express/formsExample/routes/userRouter.js
+++ full-stack/Odin Projects/full-stack-js/node_express/formsExample/routes/userRouter.js
@@ -1,9 +1,15 @@
- const { Router } = require("express");
- const usersController = require('../controllers/userController.js')
- const userRouter = Router();
- 
- userRouter.get("/", usersController.usersListGet);
- userRouter.get("/create", usersController.usersCreateGet)
- userRouter.post("/create", usersController.usersCreatePost)
- 
- module.exports = userRouter;
+const { Router } = require("express");
+const usersController = require("../controllers/userController.js");
+const userRouter = Router();
+
+userRouter.get("/", usersController.usersListGet);
+userRouter.get("/create", usersController.usersCreateGet);
+userRouter.post("/create", usersController.usersCreatePost);
+
+userRouter.get("/:id/update", usersController.usersUpdateGet);
+userRouter.post("/:id/update", usersController.usersUpdatePost);
+userRouter.post("/:id/delete", usersController.usersDeletePost);
+
+userRouter.get("/search", usersController.userSearchGet)
+
+module.exports = userRouter;
